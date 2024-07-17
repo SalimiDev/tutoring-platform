6c475c6acf014fb2f4f9d2bf281731825a4a02f5
Mehdi Salimi <Salimi.devop@gmail.com>
2024-07-18 00:40:16 +0330

    Create header and top navigation components

---
+++ src/app/_components/header/top-navigation.tsx
@@ -0,0 +1,34 @@
+'use client'; // because use want use usePathname for change style of active page
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export const TopNavigation: React.FC = () => {
+    const menuItems: NavigationMenuItem[] = [
+        { title: 'صفحه اصلی', href: '/' },
+        { title: 'دوره‌های ری‌اکت و نکست', href: '/courses' },
+        { title: 'مطالب و مقالات', href: '/blog' },
+    ];
+
+    const pathname = usePathname();
+
+    return (
+        <ul className='mr-12 flex gap-x-8'>
+            {menuItems.map((item) => {
+                const isActive = pathname === item.href;
+                return (
+                    <li key={`navigation-${item.href}`}>
+                        <Link
+                            href={item.href}
+                            className={`pb-2 transition-colors hover:text-primary ${
+                                isActive && 'border-b-2 dark:border-primary/30 dark:text-primary'
+                            }`}
+                        >
+                            {item.title}
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
