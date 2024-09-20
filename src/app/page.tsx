65c8cc73a8110130eb06255b178fed0b5d3af8da
Mehdi Salimi <Salimi.devop@gmail.com>
2024-09-20 15:00:59 +0330

    Fetch new courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import { HomeHeroSection } from './_components/home-hero-section/home-hero-section';
-export default function Home() {
+import Image from 'next/image';
+import { Button } from './_components/button/button';
+import { CourseSummary } from '@/types/course-summary.interface';
+import next from 'next';
+
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+    const res = await fetch(`https://api.classbon.com/api/courses/newest/${count}`, {
+        next: { revalidate: 24 * 60 * 60 },
+    });
+    return res.json();
+}
+
+export default async function Home() {
+    const newestCourses = await getNewestCourses(4);
     return (
         <>
             <HomeHeroSection />
+            {newestCourses.map((course) => (
+                <p key={course.title}>{course.title}</p>
+            ))}
         </>
     );
 }
